ef7a7f0f590b9822842086afa04690886eb03576
ducanh2710203 <duc2710203@gmail.com>
2023-05-15 10:39:19 +0700

    cm

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const http =
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    let txt = ""
+    if (req.url === "/login"){
+        console.log("login success")
+
+    }else {
+        console.log("login fail")
+    }
+    res.end(txt);
+})
+server.listen(8080,"localhost",()=>{
+    console.log("server dang chay")
+})
